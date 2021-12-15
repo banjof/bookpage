a13b0e059b2f71ec8ffa3652b53197534e442601
faladetimilehin <faladetimilehin@yahoo.com>
2021-12-15 07:49:16 +0100

    bookpage

---
+++ app.js
@@ -0,0 +1,35 @@
+function payWithPaystack() {
+
+    var handler = PaystackPop.setup({ 
+        key: 'pk_test_49c4e01547e6c0ba084875667e809c38b3ca0741', //put your public key here
+        email: 'opeketefoundation@gmail.com', //put your customer's email here
+        amount: 500000, //amount the customer is supposed to pay
+        metadata: {
+            custom_fields: [
+                {
+                    display_name: "Mobile Number",
+                    variable_name: "mobile_number",
+                    value: "+2348087240516" //customer's mobile number
+                }
+            ]
+        },
+        callback: function (response) {
+            //after the transaction have been completed
+            //make post call  to the server with to verify payment 
+            //using transaction reference as post data
+            $.post("verify.php", {reference:response.reference}, function(status){
+                if(status == "success")
+                    //successful transaction
+                    alert('Transaction was successful');
+                else
+                    //transaction failed
+                    alert(response);
+            });
+        },
+        onClose: function () {
+            //when the user close the payment modal
+            alert('Transaction cancelled');
+        }
+    });
+    handler.openIframe(); //open the paystack's payment modal
+}
